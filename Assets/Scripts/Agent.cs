a12b16d234d72133a9dcc1b8217d2fc8c87f9665
Fish#7793 <daveravab@gmail.com>
2022-11-18 18:16:44 -0500

    Level tools/template init

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public Vector3Int positionTarget;
+    public Vector3 eulerAngleTarget;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+
+    }
+
+    public void Rotate()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void Sense()
+    {
+
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<Tilemap> groundLayers;
+    public List<Tilemap> propLayers;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
