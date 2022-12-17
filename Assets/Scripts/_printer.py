20686ebd0ad7673cb06b893ef228bd704c9d518a
Fish#7793 <daveravab@gmail.com>
2022-12-17 02:21:45 -0500

    fixed build issue

--- Assets/Scripts/_printer.py
+++ Assets/Scripts/_printer.py
@@ -4,5 +4,5 @@ from PIL import Image
 def find(ext):
     for file in glob.glob('**/*.{}'.format(ext), recursive=True):
         print(("Scripts\\" + file).replace("\\", "/"))
-
+        print()
 find("cs")
